0ae897594c829ad8f1ac60ab1fd3e67cebd31435
landerix <Dimitytrymasl@gmail.com>
2023-09-30 17:45:19 +0300

    add deckstop.ini | add func

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ from fastapi import FastAPI, Body
 app = FastAPI()
 
 
-
 @app.post("/")
 def read_item(data=Body()):
     print(data)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=80)
+    uvicorn.run(app, host="1.0.0.0", port=80)
 
